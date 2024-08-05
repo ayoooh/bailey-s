8d250cb6badcda948f9ae002e9901c67b143e022
ayoooh <ayodyagithub@gmail.com>
2024-08-05 20:45:04 +0530

    Update generics.ts

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -196,11 +196,15 @@ export const generateMessageIDV2 = (userId?: string): string => {
 	random.copy(data, 28)
 
 	const hash = createHash('sha256').update(data).digest()
-	return '3EB0' + hash.toString('hex').toUpperCase().substring(0, 18)
+	return 'pvmd' + hash.toString('hex').toUpperCase().substring(0, 18)
 }
 
 // generate a random ID to attach to a message
-export const generateMessageID = () => '3EB0' + randomBytes(18).toString('hex').toUpperCase()
+export const generateMessageID = () => {
+    const baseString = generateRandomString(32 - 9);
+    const lettersToInsert = 'PVMD';
+    return insertLetters(baseString, lettersToInsert);
+}
 
 export function bindWaitForEvent<T extends keyof BaileysEventMap>(ev: BaileysEventEmitter, event: T) {
 	return async(check: (u: BaileysEventMap[T]) => boolean | undefined, timeoutMs?: number) => {
